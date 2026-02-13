cc465b8adbe5cdc206305389ffb82e2edc1ec773
Nechaiter <fr.esteva@duocuc.cl>
2026-02-12 22:02:26 -0300

    index to root file

--- Pages/routing.js
+++ Pages/routing.js
@@ -1,6 +1,6 @@
 function showView(view) {
   if (view === 'menu') {
-    window.location.href = '/Pages/index.html';
+    window.location.href = '/index.html';
   } else {
     window.location.href = "/Pages/"+view+'/'+view + '.html';
   }
